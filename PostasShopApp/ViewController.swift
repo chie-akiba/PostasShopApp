f7563511b2e824b0b0f0dcb97557844f501f9eb2
ITG3-00935 <chie.akiba1@gmail.com>
2020-10-26 21:29:44 +0900

    Initial Commit

---
+++ PostasShopApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostasShopApp
+//
+//  Created by ITG3-00935 on 2020/10/26.
+//  Copyright © 2020 ITG3-00935. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
